505d8f370bed543babf5ab04193bf48e5416ff3e
Ryann Goh <ryanngohenrui@gmail.com>
2026-04-12 11:34:24 +0800

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # we need to store the best window
+        bestLen = float("inf")
+        best = [-1, -1]
+
+        # build a freq map to store characters in t
+        count, window_map = {}, {}
+        for c in t:
+            count[c] = count.get(c, 0) + 1
+
+        # have: how many characters currently meet the criteria
+        # need: how many distinct characters do we still need? 
+        have, need = 0, len(count)
+
+        L = 0 
+        for R in range(len(s)):
+            window_map[s[R]] = window_map.get(s[R], 0) + 1
+            if s[R] in count and window_map[s[R]] == count[s[R]]:
+                have += 1
+
+            while have == need:
+                if (R - L + 1) < bestLen:
+                    best = [L, R]
+                    bestLen = R - L + 1
+
+                window_map[s[L]] -= 1
+                if s[L] in count and window_map[s[L]] < count[s[L]]:
+                    have -= 1
+                L += 1
+        
+        L, R = best
+        return s[L:R+1] if bestLen != float("inf") else ""
