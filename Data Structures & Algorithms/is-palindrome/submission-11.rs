9ad1bc40e022c9948a39fc56f7b967e3d7775777
Ryann Goh <ryanngohenrui@gmail.com>
2026-07-19 21:42:12 +0800

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.rs
@@ -0,0 +1,31 @@
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        let mut left = 0;
+        let mut right = s.len();
+
+        let bytes = s.as_bytes(); // More efficient for this problem
+
+        while left < right {
+            // Skip non-alphanumeric characters from left
+            while left < right && !bytes[left].is_ascii_alphanumeric() {
+                left += 1;
+            }
+            
+            // Skip non-alphanumeric characters from right
+            while left < right && !bytes[right - 1].is_ascii_alphanumeric() {
+                right -= 1;
+            }
+
+            // Compare (case-insensitive)
+            if left < right {
+                if bytes[left].to_ascii_lowercase() != bytes[right - 1].to_ascii_lowercase() {
+                    return false;
+                }
+                left += 1;
+                right -= 1;
+            }
+        }
+
+        true
+    }
+}
