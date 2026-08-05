f56e46d47cb7f090a09527d6ece3252dacbb05df
Ryann Goh <ryanngohenrui@gmail.com>
2026-08-06 00:35:08 +0800

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        used = [0] * len(nums)   # True = already chosen
+        path = []
+
+        def backtrack():
+            # Base case: we have a full permutation
+            if len(path) == len(nums):
+                res.append(path[:])   # must copy!
+                return
+
+            # Try every unused number
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+
+                # Choose
+                used[i] = 1
+                path.append(nums[i])
+
+                # Explore
+                backtrack()
+
+                # Un-choose (backtrack)
+                path.pop()
+                used[i] = 0
+
+        backtrack()
+        return res
