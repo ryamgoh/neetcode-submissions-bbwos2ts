f1d99e45787ab8533f288075a6d5f2e77097a7fe
Ryann Goh <ryanngohenrui@gmail.com>
2026-08-06 00:07:32 +0800

    Add: last-stone-weight - submission-15

---
+++ Data Structures & Algorithms/last-stone-weight/submission-15.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # at every step we get to pick 2 of the heaviest stone
+        # we should be able to get them really easily
+        # n * O(1) times
+        # O(n)
+
+        maxHeap = [-s for s in stones]
+        heapq.heapify(maxHeap)
+
+        while len(maxHeap) > 1:
+            heaviest = -heapq.heappop(maxHeap)
+            second_heaviest = -heapq.heappop(maxHeap)
+            diff = heaviest - second_heaviest
+            if diff != 0:
+                heapq.heappush(maxHeap, -diff)
+
+        if maxHeap:
+            return -maxHeap[0]
+        return 0
